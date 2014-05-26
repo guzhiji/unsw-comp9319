52cd281ffb891a6fde14b20a843767608045b773
Zhiji Gu <gu_zhiji@163.com>
2014-05-26 21:21:48 +1000

    different main entry for different builds

--- main_debug.c
+++ main_debug.c
@@ -94,49 +94,6 @@ void dump_lpos_japan() {
 
 int main(int argc, char **argv) {
 
-    unsigned char special_char = '\n'; // TODO special char='['
-    int special_char_post = 1; // TODO 0, before content
-
-    if (argc > 3) {
-        // 0: program name
-        // 1: bwt file
-        // 2: idx file
-        // 3: ?
-
-        char * opt_o = "-o";
-        bwttext * t = bwttext_init(argv[1], argv[2], special_char, 0);
-
-        if (argc > 4 && 0 == strcmp(argv[3], opt_o)) {// decoding
-            // 3: "-o"
-            // 4: unbwt file
-
-            FILE * out = fopen(argv[4], "wb");
-            decode_backward_rev(t, out);
-            fclose(out);
-        } else if (argc == 4) {// searching
-            // 3: query term
-
-            search(t, (unsigned char *) argv[3], strlen(argv[3]),
-                    special_char, special_char_post);
-
-        } else if (argc == 5) {// decoding a range
-            // 3: starting record
-            // 4: ending record
-
-            decode_range(t, atol(argv[3]), atol(argv[4]),
-                    special_char, special_char_post, stdout);
-
-        }
-
-        bwttext_free(t);
-
-        return 0;
-
-    }
-
-    // error
-    return 1;
-
     //--------------------------------------------------------------------------
     //    bwttext * t;
     //    t = bwttext_init("../tests/bwtsearch/tiny.bwt", "../tiny.idx", 1);
@@ -150,7 +107,7 @@ int main(int argc, char **argv) {
     //    bwttext_free(t);
     //    return 0;
     //--------------------------------------------------------------------------
-    //    dump_occ_japan();
-    //    dump_lpos_japan();
-    //    return 0;
+    //dump_occ_japan();
+    dump_lpos_japan();
+    return 0;
 }
